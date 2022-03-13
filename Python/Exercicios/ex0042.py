8c57077ee407a6bd579e3187ba31e2704949aec2
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2022-03-12 22:34:16 -0300

    new condition

--- Python/Exercicios/ex0042.py
+++ Python/Exercicios/ex0042.py
@@ -4,9 +4,9 @@ reta3 = int(input('Qual é o comprimento da terceira reta?  '))
 
 if reta1 + reta2 > reta3 and reta2 + reta3 > reta1 and reta3 + reta1 > reta2:
     print(' Parabens!! você terá um triangulo com os valores {}, {}  e {}'.format(reta1, reta2, reta3))
-    if reta1 == reta2 and reta2 == reta3:
+    if reta1 == reta2 == reta3:
         print('Com esses valores digitados você consegue formar um triângulo EQUILÁTERO')
-    elif reta1 != reta2 and reta2 != reta3:
+    elif reta1 != reta2 != reta3 != reta1:
         print('Com esses valores digitados você consegue formar um triângulo ESCALENO')
     else:
         print('Com esses valores digitados você consegue formar um triângulo ISÓSCELES')
