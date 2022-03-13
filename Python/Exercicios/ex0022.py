1f961c1e3851af487c0d186ff30e596eebcc9eb0
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2022-03-12 22:22:17 -0300

    up

--- Python/Exercicios/ex0022.py
+++ Python/Exercicios/ex0022.py
@@ -1,7 +1,7 @@
 nome = str(input('Qual seu nome? ')).strip()
 
 nome = nome.upper()
-print( 'Letras Maiusculas -',nome)
+print('Letras Maiusculas -',nome)
 
 nome = nome.lower()
 print('Letras Minusculas - ', nome)
@@ -10,4 +10,4 @@ print('Seu nome ao todo tem {} letras '.format(len(nome) - nome.count(' ')))
 
 nome = nome.split()
 nome = len(nome[0])
-print( 'O primeiro nome tem {} letras'.format(nome))
+print('O primeiro nome tem {} letras'.format(nome))
