632e4a012b834fd7ef9c999a03f05d902791406c
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2022-03-12 22:44:40 -0300

    new condition

--- Python/Exercicios/ex0044.py
+++ Python/Exercicios/ex0044.py
@@ -1,9 +1,12 @@
-preco = float(input('Qual é o preço do produto ?'))
-print('[1] Cartão')
-print('[2] Cartão 2x')
-print('[3] Cartão 3x')
-print('[4] Dinheiro')
-print('[5] Cheque ')
+preco = float(input('Qual é o preço do produto ? '))
+print('''
+Escolha a forma de pagamento : 
+[1] Cartão
+[2] Cartão 2x
+[3] Cartão 3x
+[4] Dinheiro
+[5] Cheque
+''')
 pagamento = int(input('Qual a forma de pagamento? '))
 if pagamento == 4 or pagamento == 5:
     valorFinal = preco - (preco * 0.1)
