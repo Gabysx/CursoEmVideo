a05d23f3a0a9ed0fbd9b3c1f0bfc806a14fe0eab
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2022-03-13 22:58:07 -0300

    archive

---
+++ Python/Exercicios/ex0052.py
@@ -0,0 +1,7 @@
+for c in range(0, 1):
+    num = int(input('Digite um numero: '))
+    if num % 2 == 0  num // 3 > 0:
+        print(f'O numero {num}  é primo')
+    else:
+        print(f'O numero {num} não é primo')
+        print('Tente Novamente')
