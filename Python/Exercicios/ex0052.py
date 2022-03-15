cfaeb1e117ac056bc8b45ce0dd992a8837132706
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2022-03-14 23:05:05 -0300

    up

--- Python/Exercicios/ex0052.py
+++ Python/Exercicios/ex0052.py
@@ -1,7 +1,13 @@
-for c in range(0, 1):
-    num = int(input('Digite um numero: '))
-    if num % 2 == 0  num // 3 > 0:
-        print(f'O numero {num}  é primo')
+num = int(input('Digite um numero: '))
+soma = 0
+for c in range(1, num + 1):
+    if num % c == 0:
+        print('\033[34m')
+        soma += 1
     else:
-        print(f'O numero {num} não é primo')
-        print('Tente Novamente')
+        print('\033[32m')
+if soma == 2:
+    print(f'O numero {num} é primo')
+else:
+    print(f'O numero {num} não é primo')
+    print('Tente Novamente')
