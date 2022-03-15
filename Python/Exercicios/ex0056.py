e301464298bdb1dd31334ce5ec62fc2d93881efa
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2022-03-14 23:14:16 -0300

    up

--- Python/Exercicios/ex0056.py
+++ Python/Exercicios/ex0056.py
@@ -4,8 +4,9 @@ mulheres = 0
 velho = 0
 esc = ''
 
-for c in range(0, 4):
-    nome = str(input('Digite um nome : '))
+for c in range(1, 5):
+    print(f'----- {c}º PESSOA -----')
+    nome = str(input('Digite um nome : ')).strip()
     idade = int(input('Digite a idade: '))
     print('''1 - [FEM] ou 2 - [MASC]''')
     sexo = int(input('Qual é o sexo ? '))
@@ -20,7 +21,7 @@ for c in range(0, 4):
 
     mediaIdade += idade / 4
 
-print(f'Essa é a média {mediaIdade:.2f} de idade dessas pessoas ')
+print(f'A média  de idade desse grupo é de  {mediaIdade:.2f} anos')
 print(f' {mulheres} é a quantidade de mulheres com idades abaixo de 20 anos')
-print(f'{esc.capitalize()} é o cara mais velho da turma')
+print(f'{esc.capitalize()} é o cara mais velho da turma com {velho} anos')
 
