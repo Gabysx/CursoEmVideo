f3580ac8aa1a69ff8bb2e058d71fb5647a306eea
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2022-03-14 23:06:17 -0300

    up

--- Python/Exercicios/ex0054.py
+++ Python/Exercicios/ex0054.py
@@ -5,9 +5,9 @@ maior = 0
 for c in range(0, 7):
     num = int(input('Qual seu ano de nascimento ? '))
     calc = anoAtual - num
-    if calc < 21:
+    if calc <= 21:
         menor += 1
-    elif calc > 21:
+    elif calc >= 21:
         maior += 1
 print(f'Quantidade de pessoas maiores de idade são {maior}')
 print(f'Quantidade de pessoas menores de idade são {menor}')
