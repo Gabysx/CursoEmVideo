fa70b04e7e5e89862f07ea372d45e45538c67d18
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2022-03-16 23:53:57 -0300

    Jogo Advinha While

---
+++ Python/Exercicios/EX0058.py
@@ -0,0 +1,24 @@
+from random import randint
+from time import sleep
+jogadas = 0
+acertos = 0
+perdas = 0
+while jogadas < 10:
+    print('Escolha um numero entre 0 e 10')
+    print('=-' * 20)
+    computador = randint(0, 10)
+    num = int(input('Qual é o numero que deseja escolher ? '))
+    print('Processando...')
+    sleep(0.5)
+
+    if num == computador:
+        print('PARABENS VC ACERTOU')
+        acertos += 1
+    else:
+        print('Que pena vc perdeu, tente novamente !! ')
+        print('')
+        perdas += 1
+
+    jogadas += 1
+
+print(f'Você ganhou {acertos} vezes do computador e perdeu {perdas} vezes para ele !!')
