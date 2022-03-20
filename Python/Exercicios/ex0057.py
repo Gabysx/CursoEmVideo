aa384c4aab7a431dfdfbeace2203502fe165b5a5
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2022-03-19 21:42:29 -0300

    up

--- Python/Exercicios/ex0057.py
+++ Python/Exercicios/ex0057.py
@@ -1,10 +1,9 @@
 sexo = ''
 while sexo != 'F' and sexo != 'M':
-    sexo = str(input('Digite seu sexo [F] ou [M]: ')).upper()
-    print(sexo)
-
-if sexo == 'F':
-    print(f'Seu sexo é FEMININO')
-else:
-    print(f'Seu sexo é MASCULINO')
-
+    sexo = str(input('Digite seu sexo [F/M]: ')).strip().upper()[0]
+    if sexo == 'F':
+        print(f'Seu sexo é FEMININO e foi registrado com sucesso.')
+    elif sexo == 'M':
+        print(f'Seu sexo é MASCULINO e foi registrado com sucesso. ')
+    else:
+        print('Ops!! Dados inválidos tente novamente. ', end='')
