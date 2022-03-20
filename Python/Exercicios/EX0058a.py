5558315a43be527dc65f78664f561963071e8d74
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2022-03-19 21:41:07 -0300

    rename

--- Python/Exercicios/EX0058a.py
+++ Python/Exercicios/EX0058a.py
@@ -3,9 +3,10 @@ from time import sleep
 jogadas = 0
 acertos = 0
 perdas = 0
+print('Escolha um numero entre 0 e 10')
+print('=-' * 20)
 while jogadas < 10:
-    print('Escolha um numero entre 0 e 10')
-    print('=-' * 20)
+
     computador = randint(0, 10)
     num = int(input('Qual é o numero que deseja escolher ? '))
     print('Processando...')
@@ -19,6 +20,6 @@ while jogadas < 10:
         print('')
         perdas += 1
 
-    jogadas += 1
+        jogadas += 1
 
 print(f'Você ganhou {acertos} vezes do computador e perdeu {perdas} vezes para ele !!')
