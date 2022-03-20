73adb180d14d22e0f98d05204de7742b19bd77b3
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2022-03-19 21:43:09 -0300

    up

--- Python/Exercicios/ex0059.py
+++ Python/Exercicios/ex0059.py
@@ -1,10 +1,9 @@
 menu = 0
-while menu != 5:
-    print('')
-    print('-' * 5 + ' Vamos Brincar ' + '-' * 5)
-    num1 = float(input('Digite um número: '))
-    num2 = float(input('Digite um número: '))
+print('-' * 5 + ' Vamos Brincar ' + '-' * 5)
+num1 = float(input('Primeiro numero: '))
+num2 = float(input('Segundo numero: '))
 
+while menu != 5:
     print(f''' 
     {'-' * 3} Opções: {'-' * 3}
     [1] SOMAR
@@ -13,35 +12,28 @@ while menu != 5:
     [4] NOVOS NUMEROS 
     [5] SAIR DO PROGRAMA 
     ''')
-    resp = int(input('Escolha uma opção: '))
+    menu = int(input('Qual é a sua opção: '))
 
-    if resp == 1:
+    if menu == 1:
         soma = num1 + num2
         print(f'Obtem-se o valor de {soma} com a soma dos valores {num1} e {num2}.')
-    if resp == 2:
+    elif menu == 2:
         mult = num1 * num2
         print(f'Obtem-se o valor de {mult} com a multiplicação dos valores {num1} e {num2}.')
-    if resp == 3:
+    elif menu == 3:
         if num1 > num2:
             print(f'{num1} é o maior número digitado')
         elif num2 > num1:
             print(f'{num2} é o maior número digitado')
         else:
             print(f'Os numeros digitados são iguais... Tente novamente')
+    elif menu == 4:
+        print('Trocar os numeros')
+        num1 = float(input('Primeiro numero : '))
+        num2 = float(input('Segundo numero: '))
 
-    if resp == 4:
-        num1 = float(input('Digite outro número: '))
-        num2 = float(input('Digite mais outro número: '))
-        print(f''' 
-           {'-' * 3} Opções: {'-' * 3}
-           [1] SOMAR
-           [2] MULTIPLICAR
-           [3] MAIOR
-           [4] NOVOS NUMEROS 
-           [5] SAIR DO PROGRAMA 
-           ''')
-        resp = int(input('Escolha uma opção: '))
-
-    if resp == 5:
+    elif menu == 5:
         print('')
         print('Muito Obrigado por se divertir conosco. Até a próxima !')
+    else:
+        print('Opção inválida tente novamente... ')
