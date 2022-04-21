c43b708ab824973c132e073fa1bbd048277b42bc
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2022-04-21 19:01:07 -0300

    up

--- Python/Exercicios/ex0065.py
+++ Python/Exercicios/ex0065.py
@@ -1,18 +1,26 @@
 c = 0
 soma = media = maior = menor = 0
 resp = ''
+com = str(input('Vamos começar? [S/N] ')).strip().upper()
 while resp != 'N':
-    num = int(input('Digite um valor: '))
-    resp = str(input('Deseja continuar? [N/S]')).strip().upper()
-    soma += num
-    if num > maior:
-        maior = num
-    if maior < num:
-        menor = num
-    c += 1
-    media = soma / c
-print(media)
-print(maior)
-print(menor)
-print(c)
-print(soma)
+    if com in 'S':
+        num = int(input('Digite um valor: '))
+        resp = str(input('Deseja continuar? [N/S]: ')).strip().upper()
+
+        if num > maior:
+            maior = num
+
+        if num < maior:
+            menor = num
+
+        c += 1
+        soma += num
+        media = soma / c
+    else:
+        print(20*'*=*')
+        print('Resposta incorreta !! ')
+        resp = 'N'
+
+print(f'Foram digitados {c} numeros e a média entre eles foi {media:.2f}')
+print(f'Sendo que o maior numero digitado foi {maior} e o menor numero digitado foi {menor}')
+print(f'A soma entre os numeros digitados é de {soma}')
