2acb87dfa6f3c4b4b8e3c4ae35e8bf3993d50bdb
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2022-05-14 12:28:05 -0300

    Hora

---
+++ JavaScript/ModuloD/ex0012.js
@@ -0,0 +1,13 @@
+var agora = new Date();
+var hora = agora.getHours()
+var  min = agora.getMinutes()
+console.log(`Agora são ${hora+ ':' + min} .`)
+
+if (hora < 12){
+  console.log('Bom dia!')
+}
+else if (hora <= 18){
+  console.log('Boa Tarde!')
+} else {
+  console.log('Boa Noite!')
+}
