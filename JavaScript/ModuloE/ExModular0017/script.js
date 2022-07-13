caec708d47e442ec901a9de68d223c9f94f56716
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2022-07-13 17:49:57 -0300

    new

--- JavaScript/ModuloE/ExModular0017/script.js
+++ JavaScript/ModuloE/ExModular0017/script.js
@@ -1,19 +1,11 @@
 function gerar() {
-  var valor1 = document.getElementById('valor1')
-  var tab = document.getElementById('seltab')
+  var valor1 = [document.getElementById('valor1')]
+  var tab = [document.getElementById('seltab')]
 
   if (valor1.value.length == 0){
     window.alert('Por favor, digite uma número!')
   } else {
     var num = Number(valor1.value)
-    var cont = 1
-    tab.innerHTML = ''
-    while(cont <= 10){
-      var item = document.createElement('option')
-      item.text = ` ${num} X ${cont} = ${num * cont} ` 
-      item.value = `tab ${cont}`
-      tab.appendChild(item)
-      cont ++
-    }
+    
   }
 }
